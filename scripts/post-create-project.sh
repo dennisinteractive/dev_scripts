40fb3906cb5dbe30917362fcdb3fddb3550d2ee5
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-10-29 00:46:08 +0100

    Fixing scripts

--- scripts/post-create-project.sh
+++ scripts/post-create-project.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-ls
+pwd=`pwd`
 
 # Remove existing symlink
 if [ -L /usr/local/bin/drupal ]; then sudo rm /usr/local/bin/drupal; fi
 # Create symlink
-echo "[Info] Creating symlink"
-echo "Type your password"
-sudo ln -s ./vendor/bin/drupal /usr/local/bin/drupal
+echo "[Info] Creating symlink. You might need to type your password to use sudo"
+sudo ln -s ${pwd}/vendor/bin/drupal /usr/local/bin/drupal
 
 # Drupal commands
 echo "[Info] Drupal init"
 drupal init
 
 # Append custom config.yml to the global config
-cat ./vendor/dennisdigital/drupal_console_commands/config.yml >> ~/.console/config.yml
+cat ${pwd}/vendor/dennisdigital/drupal_console_commands/config.yml >> ~/.console/config.yml
