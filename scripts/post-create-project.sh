8149bd8c15526493088f27e0107cf04f990e6a27
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-10-28 13:03:36 +0100

    Added messages

--- scripts/post-create-project.sh
+++ scripts/post-create-project.sh
@@ -3,9 +3,11 @@
 # Remove existing symlink
 if [ -L /usr/local/bin/drupal ]; then sudo rm /usr/local/bin/drupal; fi
 # Create symlink
+echo "[Info] Creating symlink"
 sudo ln -s ./vendor/bin/drupal /usr/local/bin/drupal
 
 # Drupal commands
+echo "[Info] Drupal init"
 drupal init
 
 # Append custom config.yml to the global config
