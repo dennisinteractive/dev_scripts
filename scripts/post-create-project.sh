886d79357d95e842360776c85036cb05014de458
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-10-28 14:05:44 +0100

    Display messsage

--- scripts/post-create-project.sh
+++ scripts/post-create-project.sh
@@ -5,6 +5,7 @@ ls
 if [ -L /usr/local/bin/drupal ]; then sudo rm /usr/local/bin/drupal; fi
 # Create symlink
 echo "[Info] Creating symlink"
+echo "Type your password"
 sudo ln -s ./vendor/bin/drupal /usr/local/bin/drupal
 
 # Drupal commands
