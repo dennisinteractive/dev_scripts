dad6fa9bf29e598f2a030d07bc620e9a32929c21
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-11-21 15:45:17 +0000

    Run later

--- scripts/post-create-project.sh
+++ scripts/post-create-project.sh
@@ -10,12 +10,13 @@ sudo ln -s ${pwd}/vendor/bin/drupal /usr/local/bin/drupal
 # Drupal commands
 echo "[Info] Drupal init"
 drupal init --override -e dev
-# -e dev is not working, it should change environment to dev, workaround
-sed -i -e 's/environment: prod/environment: dev/' ~/.console/config.yml
-sed -i -e "s/environment: 'prod'/environment: 'dev'/" ~/.console/config.yml
 
 # Append custom config.yml to the global config
 cat ${pwd}/vendor/dennisdigital/drupal_console_commands/config.yml >> ~/.console/config.yml
+
+# -e dev is not working, it should change environment to dev, workaround
+sed -i -e 's/environment: prod/environment: dev/' ~/.console/config.yml
+sed -i -e "s/environment: 'prod'/environment: 'dev'/" ~/.console/config.yml
 
 # Copy chain.yml to home folder
 cp ${pwd}/vendor/dennisdigital/drupal_console_commands/chain.yml ~/.console/
