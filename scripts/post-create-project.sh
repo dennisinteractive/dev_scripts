49c1609528dc4b4d38252a81b858ddd13a45eabe
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-11-21 15:21:39 +0000

    Set environment to dev on vms

--- scripts/post-create-project.sh
+++ scripts/post-create-project.sh
@@ -9,7 +9,7 @@ sudo ln -s ${pwd}/vendor/bin/drupal /usr/local/bin/drupal
 
 # Drupal commands
 echo "[Info] Drupal init"
-drupal init
+drupal init --override -e dev
 
 # Append custom config.yml to the global config
 cat ${pwd}/vendor/dennisdigital/drupal_console_commands/config.yml >> ~/.console/config.yml
